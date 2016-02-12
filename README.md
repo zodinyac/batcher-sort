2a0d7dedd5a0f5a912ec1eac0f5df0294b387ed9
Nikita Belov <zodiac.nv@gmail.com>
2016-02-12 22:18:02 +0300

    Added generator.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # batcher-sort
 Implementation of Batcher's sort using MPI.
+
+### Generator
+Generates a file with random sequence.
+Usage:
+```sh
+./generator <output.dat> <count of elements>
+```
---
+++ generator/generator.c
@@ -0,0 +1,37 @@
+﻿/*
+ * Generates a file with random sequence.
+ * Nikita Belov <zodiac.nv@gmail.com>
+ * 2016
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: generator <output.dat> <count>.\n");
+        return 1;
+    }
+    
+    int count = strtol(argv[2], NULL, 10);
+    
+    FILE *output = fopen(argv[1], "wb");
+    if (output == NULL) {
+        printf("File '%s' can't be opened.\n", argv[1]);
+        return 2;
+    }
+    
+    srand(time(NULL));
+    
+    fwrite(&count, sizeof(count), 1, output);
+    for (int i = 0; i < count; i++) {
+        int value = rand();
+        fwrite(&value, sizeof(value), 1, output);
+    }
+    
+    fclose(output);
+    
+    return 0;
+}
